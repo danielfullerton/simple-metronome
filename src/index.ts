f6ed530948d3d76b921b73a58cd643b5d55a359a
Daniel <dan.frank.fullerton@gmail.com>
2020-06-15 18:29:59 -0400

    Fixed bug with multi-click on start

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import * as Tone from 'tone';
 
-const BPM = 300;
+const BPM = 120;
 
 let agg = 0;
 let count = 1;
 let inc_constant = 100;
 let interval: NodeJS.Timer;
+let playing = false;
 
 const high = new Tone.Player('assets/sounds/high_clave.wav').toMaster();
 const low = new Tone.Player('assets/sounds/low_clave.wav').toMaster();
 
 const startMet = () => {
+  if (playing) return;
+  playing = true;
   interval = setInterval(() => {
     agg+= inc_constant;
     if (agg >=((60 * 1000) / BPM)) {
@@ -26,9 +29,12 @@ const startMet = () => {
 };
 
 const stopMet = () => {
+  playing = false;
+  count = 1;
+  high.stop();
+  low.stop();
   clearInterval(interval);
 };
-
 
 const start: HTMLElement = document.getElementById('start');
 start.onclick = () => {
@@ -39,7 +45,5 @@ const stop = document.getElementById('stop');
 stop.onclick = () => {
   stopMet();
 }
-
-
 
 // todo: credit https://freesfx.co.uk
