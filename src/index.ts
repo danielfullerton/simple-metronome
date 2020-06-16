fb73eaa03f1d7eafd814bb42f8d1740adf391fba
Daniel <dan.frank.fullerton@gmail.com>
2020-06-15 20:54:24 -0400

    Added very basic input box for variable bpm

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ document.getElementById('stop').onclick = () => {
   metronome.stop();
 }
 
+document.getElementById('bpm').onchange = (e: any) => {
+  metronome.setTempo(Number.parseInt(e.target.value));
+}
+
 window.addEventListener('keypress', e => {
   if (e.key === ' ') {
     metronome.toggle();
