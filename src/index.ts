f29bc8924e0c2879b5edd12f001cd073481fd869
Daniel <dan.frank.fullerton@gmail.com>
2020-06-15 20:43:44 -0400

    Refactored metronome into a class

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,19 @@
-import * as Tone from 'tone';
+import { Metronome } from './lib/Metronome';
 
-const BPM = 120;
+const metronome = new Metronome();
 
-let agg = 0;
-let count = 1;
-let inc_constant = 100;
-let interval: NodeJS.Timer;
-let playing = false;
-
-const high = new Tone.Player('assets/sounds/high_clave.wav').toMaster();
-const low = new Tone.Player('assets/sounds/low_clave.wav').toMaster();
-
-const startMet = () => {
-  if (playing) return;
-  playing = true;
-  interval = setInterval(() => {
-    agg+= inc_constant;
-    if (agg >=((60 * 1000) / BPM)) {
-      if (count === 1) {
-        high.start();
-      } else {
-        low.start();
-      }
-      count = count >= 4 ? 1 : count + 1;
-      agg = 0;
-    }
-  }, inc_constant);
-};
-
-const stopMet = () => {
-  playing = false;
-  count = 1;
-  high.stop();
-  low.stop();
-  clearInterval(interval);
-};
-
-const start: HTMLElement = document.getElementById('start');
-start.onclick = () => {
-  startMet();
-};
-
-const stop = document.getElementById('stop');
-stop.onclick = () => {
-  stopMet();
+document.getElementById('start').onclick = () => {
+  metronome.start();
 }
+
+document.getElementById('stop').onclick = () => {
+  metronome.stop();
+}
+
+window.addEventListener('keypress', e => {
+  if (e.key === ' ') {
+    metronome.toggle();
+  }
+});
 
 // todo: credit https://freesfx.co.uk
---
+++ src/lib/Metronome.ts
@@ -0,0 +1,81 @@
+import * as Tone from 'tone';
+
+export class Metronome {
+  private tempo = 120;
+  private noteCount = 1;
+  private readonly incrementConstant = 100;
+  private playing = false;
+  private stopped = true;
+  private aggregator = 0;
+  private notesPerBar = 4;
+
+  private highTone: any = new Tone.Player('assets/sounds/high_clave.wav').toMaster();
+  private lowTone: any = new Tone.Player('assets/sounds/low_clave.wav').toMaster();
+
+  start () {
+    if (this.playing) {
+      return;
+    }
+    this.playing = true;
+    this.stopped = false;
+    this.play(true);
+  }
+
+  play (first = true) {
+    if (this.stopped) {
+      return;
+    }
+
+    // todo: can we reduce or remove latency between intervals? Drift correction works for aggregation timing, not so much for consistent interval timing.
+    this.aggregator += this.incrementConstant;
+    if (this.aggregator >= (60000 / this.tempo) || first) {
+      this.playNote();
+      this.noteCount = this.noteCount >=this.notesPerBar ? 1 : this.noteCount + 1;
+      this.aggregator = 0;
+    }
+    setTimeout(() => this.play(false), this.incrementConstant);
+  }
+
+  stop () {
+    this.playing = false;
+    this.stopped = true;
+    this.count(true);
+  }
+
+  toggle () {
+    if (this.stopped) {
+      this.start();
+    } else {
+      this.stop();
+    }
+  }
+
+  playNote () {
+    if (this.noteCount === 1) {
+      this.highTone.start();
+    } else {
+      this.lowTone.start();
+    }
+  }
+
+  count (reset = false) {
+    if (reset) {
+      this.noteCount = 1;
+      return;
+    }
+
+    this.noteCount++;
+  }
+
+  setTempo (tempo: number) {
+    if (tempo < 30 || tempo > 300) {
+      throw new Error('Cannot set tempo above 300 or below 30!');
+    }
+
+    this.tempo = tempo;
+  }
+
+  getTempo () {
+    return this.tempo;
+  }
+}
